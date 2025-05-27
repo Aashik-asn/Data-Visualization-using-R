7aebc260bb1cafcf4175217eacb5ad78388edbfb
Aashik Afshan A S <aashikrasheed786@gmail.com>
2025-05-27 17:17:39 +0530

    Add files via upload

---
+++ K nearest_iris.R
@@ -0,0 +1,60 @@
+install.packages("caTools")
+insatll.packages("class")
+library(e1071)
+library(caTools)
+library(class)
+library(ggplot2)
+
+iris<-read.csv("C:/External/Nanna/Code/R/dataset/iris.csv")
+head(iris)
+tail(iris)
+str(iris)
+
+split<-sample.split(iris,SplitRatio=0.7)
+train_cl=subset(iris,split==TRUE)
+test_cl=subset(iris,split==FALSE)
+
+train_scale=scale(train_cl[,1:4])
+test_scale=scale(test_cl[,1:4])
+
+knn_classifier<-knn(train=train_scale,test=test_scale,cl=train_cl$variety,k=1)
+knn_classifier
+cm<-table(test_cl$variety,knn_classifier)
+cm          
+misClassError=mean(knn_classifier!=test_cl$variety)
+print(paste("Accuracy:",1 - misClassError))
+
+knn_classifier<-knn(train=train_scale,test=test_scale,cl=train_cl$variety,k=3)
+misClassError=mean(knn_classifier!=test_cl$variety)
+print(paste("Accuracy:",1 - misClassError))
+
+knn_classifier<-knn(train=train_scale,test=test_scale,cl=train_cl$variety,k=5)
+misClassError=mean(knn_classifier!=test_cl$variety)
+print(paste("Accuracy:",1 - misClassError))
+
+knn_classifier<-knn(train=train_scale,test=test_scale,cl=train_cl$variety,k=7)
+misClassError=mean(knn_classifier!=test_cl$variety)
+print(paste("Accuracy:",1 - misClassError))
+
+knn_classifier<-knn(train=train_scale,test=test_scale,cl=train_cl$variety,k=15)
+misClassError=mean(knn_classifier!=test_cl$variety)
+print(paste("Accuracy:",1 - misClassError))
+
+knn_classifier<-knn(train=train_scale,test=test_scale,cl=train_cl$variety,k=19)
+misClassError=mean(knn_classifier!=test_cl$variety)
+print(paste("Accuracy:",1 - misClassError))
+
+k_values=c(1,3,5,7,15,19)
+accuracy<-sapply(k_values,function(k){
+  knn_classifier<-knn(train=train_scale,test=test_scale,cl=train_cl$variety,k=k)
+  1-mean(knn_classifier!=test_cl$variety)
+})
+dev.off()#To close current graphical device(use to display the below graph without errors)
+accuracy_data=data.frame(k=k_values,accuracy=accuracy)
+ggplot(accuracy_data,aes(x=k,y=accuracy))+
+         geom_line(color="green",size=1)+
+         geom_point(color="blue",size=3)+
+         labs(title="K nearest Alg",x="K Values",y="Accuracy")+
+         theme_minimal()
+       
+       
---
+++ naiveBayes_titanic.R
@@ -0,0 +1,40 @@
+library(e1071)
+library(ggplot2)
+library(caret)
+library(dplyr)
+
+titanic<-read.csv("C:/External/Nanna/Code/R/dataset/titanic.csv")
+head(titanic)
+tail(titanic)
+str(titanic)
+
+titanic=select(titanic,Survived,Pclass,Sex,SibSp,Parch)
+titanic=na.omit(titanic)
+
+titanic$Pclass=factor(titanic$Pclass,order=TRUE,levels=c(3,2,1))
+titanic$Survived=factor(titanic$Survived)
+
+ggplot(titanic,aes(x=Survived))+
+  geom_bar(width=0.5,fill="coral")+
+  geom_text(stat='count',aes(label=stat(count)),vjust=-0.5)+theme_classic()
+
+train_test_split=function(data,fraction=0.8,train=TRUE){
+  tot_rows=nrow(data)
+  train_rows=fraction*tot_rows
+  sample=1:train_rows
+  if(train==TRUE){
+    return (data[sample,])
+  }
+  else{
+    return (data[-sample,])
+  }
+}
+train_data=train_test_split(titanic,0.8,train=TRUE)
+test_data=train_test_split(titanic,0.8,train=FALSE)
+fit=naiveBayes(Survived~.,data=train_data,method="class")
+predicted=predict(fit,test_data,type="class")
+
+table=table(test_data$Survived,predicted)
+table
+accuracy=sum(diag(table))/sum(table)
+print(paste("Accuracy: ",accuracy))
